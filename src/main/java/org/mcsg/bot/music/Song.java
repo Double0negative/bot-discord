5ac32e3a48087158d8576386b89753b513da6707
Double0negative <drew@superleague.com>
2017-05-24 12:17:23 -0700

    Clears, embeds and updates

---
+++ src/main/java/org/mcsg/bot/music/Song.java
@@ -0,0 +1,27 @@
+package org.mcsg.bot.music;
+
+import org.mcsg.bot.api.BotUser;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+public class Song {
+
+	private AudioTrack track;
+	private BotUser player;
+	private String query;
+	
+	private int likes;
+	private int dislikes;
+	
+	private long length;
+	
+	
+	public Song(AudioTrack track, String query, BotUser player) {
+		this.track = track;
+		this.query = query;
+		this.player = player;
+	}
+	
+	
+	
+}
